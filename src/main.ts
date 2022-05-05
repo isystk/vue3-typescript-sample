d8259e93fc58dbce2d391b885b67c20dd04a720a
iseyoshitaka <ise0615@gmail.com>
2022-05-05 19:58:42 +0900

    setup sass

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {createApp} from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
 import routes from 'pages-generated'
-import './index.css'
+import './styles/app.scss'
 
 const router = createRouter({
   history: createWebHistory(),
