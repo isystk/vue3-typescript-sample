ec613b328a7b18333d512b79efbe5c5769d49107
iseyoshitaka <ise0615@gmail.com>
2022-05-06 22:51:06 +0900

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
 import routes from 'pages-generated'
 import './styles/app.scss'
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+const vuetify = createVuetify({ components, directives });
+app.use(router);
+app.use(vuetify);
+app.mount("#app");
