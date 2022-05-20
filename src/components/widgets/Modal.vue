0709070be0606f59c70ae59625960dca07938b4d
iseyoshitaka <ise0615@gmail.com>
2022-05-20 19:00:26 +0900

    post form

--- src/components/widgets/Modal.vue
+++ src/components/widgets/Modal.vue
@@ -1,20 +1,33 @@
 <template>
   <v-dialog v-model="_isOpen">
-    <v-card>
-      <v-card-text>
-        <slot />
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="primary" block @click="handleClose">Close Dialog</v-btn>
-      </v-card-actions>
-    </v-card>
+    <v-container>
+      <v-card class="md:max-w-lg" >
+        <v-toolbar
+            dark
+            color="primary"
+        >
+          <v-toolbar-title>{{title}}</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn
+              icon
+              dark
+              @click="handleClose"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-card-text>
+          <slot />
+        </v-card-text>
+      </v-card>
+    </v-container>
   </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
-
 const props = defineProps<{
+  title: string
   isOpen: boolean
   handleClose: () => void
 }>()
--- src/components/widgets/PostRegistModal.vue
+++ src/components/widgets/PostRegistModal.vue
@@ -1,11 +1,95 @@
 <template>
-  <Modal :isOpen="isOpen" :handleClose="handleClose"> hoge </Modal>
+  <Modal title="投稿フォーム" :isOpen="isOpen" :handleClose="handleClose">
+    <VeeForm
+        v-slot="{ errors }"
+        :validation-schema="schema"
+        @submit="onSubmit"
+      >
+        <div class="mt">
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2">
+              メールアドレス
+            </label>
+            <Field
+              name="email"
+              type="text"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              :class="{ 'is-invalid': errors.email }"
+            />
+            <ErrorMessage class="text-red" name="email" />
+          </div>
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2">
+              パスワード
+            </label>
+            <Field
+              name="password"
+              type="password"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              :class="{ 'is-invalid': errors.password }"
+            />
+            <ErrorMessage class="text-red" name="password" />
+          </div>
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2">
+              パスワード (確認)
+            </label>
+            <Field
+              name="confirmPassword"
+              type="password"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              :class="{ 'is-invalid': errors.confirmPassword }"
+            />
+            <ErrorMessage class="text-red" name="confirmPassword" />
+          </div>
+          <div class="mb-4">
+            <v-btn depressed color="primary" type="submit"> 登録 </v-btn>
+          </div>
+        </div>
+      </VeeForm>
+  </Modal>
 </template>
 
 <script setup lang="ts">
 import Modal from '@/components/widgets/Modal.vue'
+import { Form as VeeForm, Field, ErrorMessage } from 'vee-validate'
+import * as Yup from 'yup'
+import { injectStore } from '@/store'
+import { Url } from '@/constants/url'
+import router from '@/router'
+const main = injectStore()
 const props = defineProps<{
   isOpen: boolean
   handleClose: () => void
 }>()
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+      .required('メールアドレスを入力してください')
+      .email('メールアドレスを正しく入力してください'),
+  password: Yup.string()
+      .min(6, 'パスワードは6文字以上で入力して下さい')
+      .required('パスワードを入力してください'),
+  confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'パスワードが一致しません')
+      .required('パスワード(確認)を入力してください'),
+})
+
+type FormValues = {
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+const onSubmit = async (values: FormValues) => {
+  try {
+    console.log(values)
+    const { email, password } = values
+    const result = await main?.auth.signUp(email, password)
+    console.log(result)
+    await router.push(Url.SIGNUP_VERIFICATION)
+  } catch (e) {
+    alert(e.message)
+  }
+}
 </script>
